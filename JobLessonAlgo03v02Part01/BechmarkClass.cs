817709a279370d4624d4a76dba4b0465c37ada16
safroniv <safroniv@mail.ru>
2022-05-24 22:57:56 +0300

    GB - Алгоритмы. Урок 3 Задание 1 - редакция 01

--- JobLessonAlgo03v02Part01/BechmarkClass.cs
+++ JobLessonAlgo03v02Part01/BechmarkClass.cs
@@ -2,82 +2,132 @@
 
 namespace JobLessonAlgo03v02Part01
 {
-    public class BechmarkClass
-    {
-
+    public partial class BechmarkClass
+    { 
         /// <summary>
-        /// Класс ссылочного типа обозначения координат точки типа float
+        /// Метод генерации случайных значений координат
+        /// и расчёт дистанции для класса со значениями типа Float
         /// </summary>
-        public class PointClassFloat
+        static void DistanceClassFloat()
         {
-            public float X;
-            public float Y;
+            var x1 = new Random();
+            var y1 = new Random();
+
+            var x2 = new Random();
+            var y2 = new Random();
+
+            PointClassFloat pointClassFloat1 = new()
+            { X = x1.NextSingle(), Y = y1.NextSingle() };
+
+            PointClassFloat pointClassFloat2 = new()
+            { X = x2.NextSingle(), Y = y2.NextSingle() };
+
+            PointDistanceClassFloat(pointClassFloat1, pointClassFloat2);
         }
 
         /// <summary>
-        /// Значимый тип обозначения координат точки типа float
+        /// Метод генерации случайных значений координат
+        /// и расчёт дистанции для структуры со значениями типа Float
         /// </summary>
-        public struct PointStructFloat
+        static void DistanceStructFloat()
         {
-            public float X;
-            public float Y;
+            var x3 = new Random();
+            var y3 = new Random();
+
+            var x4 = new Random();
+            var y4 = new Random();
+
+            PointStructFloat pointStructFloat1 = new()
+            { X = x3.NextSingle(), Y = y3.NextSingle() };
+
+            PointStructFloat pointStructFloat2 = new()
+            { X = x4.NextSingle(), Y = y4.NextSingle() };
+
+            PointDistanceStructFloat(pointStructFloat1, pointStructFloat2);
+
         }
 
         /// <summary>
-        /// Значимый тип обозначения координат точки типа Double
+        /// Метод генерации случайных значений координат
+        /// и расчёт дистанции для структуры со значениями типа Double
         /// </summary>
-        public struct PointStructDouble
+        static void DistanceStructDouble()
         {
-            public double X;
-            public double Y;
+            var x5 = new Random();
+            var y5 = new Random();
+
+            var x6 = new Random();
+            var y6 = new Random();
+
+            PointStructDouble pointStructDouble1 = new()
+            { X = x5.NextDouble(), Y = y5.NextDouble() };
+
+            PointStructDouble pointStructDouble2 = new()
+            { X = x6.NextDouble(), Y = y6.NextDouble() };
+
+            PointDistanceStructDouble(pointStructDouble1, pointStructDouble2);
+
         }
 
         /// <summary>
-        /// Значимый тип обозначения координат точки типа float 
+        /// Метод генерации случайных значений координат
+        /// и расчёт дистанции для структуры со значениями типа Float
         /// без расчёта квадратного корня
         /// </summary>
-        public struct PointStructWithoutSQRT
+        static void DistanceStructFloatWothoutSQRT()
         {
-            public float X;
-            public float Y;
+            var x7 = new Random();
+            var y7 = new Random();
+
+            var x8 = new Random();
+            var y8 = new Random();
+
+            PointStructWithoutSQRT pointStructWithoutSQRT1 = new()
+            { X = x7.NextSingle(), Y = y7.NextSingle() };
+
+            PointStructWithoutSQRT pointStructWithoutSQRT2 = new()
+            { X = x8.NextSingle(), Y = y8.NextSingle() };
+
+            PointDistanceStructWithoutSQRT(pointStructWithoutSQRT1, pointStructWithoutSQRT2);
         }
 
         /// <summary>
         /// Метод расчёта расстояния между точкама для ссылочных типов Float
         /// </summary>
-        /// <param name="pointOne">Первая точка</param>
-        /// <param name="pointTwo">Вторая точка</param>
+        /// <param name="pointClassFloatOne">Первая точка</param>
+        /// <param name="pointClassFloatTwo">Вторая точка</param>
         /// <returns>Возвращает расстояние</returns>
-        public static float PointDistanceFloat(PointClassFloat pointOne, PointClassFloat pointTwo)
+        public static float PointDistanceClassFloat(PointClassFloat pointClassFloatOne, PointClassFloat pointClassFloatTwo)
         {
-            float x = pointOne.X - pointTwo.X;
-            float y = pointOne.Y - pointTwo.Y;
+
+            float x = pointClassFloatOne.X - pointClassFloatTwo.X;
+            float y = pointClassFloatOne.Y - pointClassFloatTwo.Y;
             return MathF.Sqrt((x * x) + (y * y));
         }
 
         /// <summary>
         /// Метод расчёта расстояния между точкама для значимых типов Float
         /// </summary>
-        /// <param name="pointOne">Первая точка</param>
-        /// <param name="pointTwo">Вторая точка</param>
+        /// <param name="pointStructFloatOne">Первая точка</param>
+        /// <param name="pointStructFloatTwo">Вторая точка</param>
         /// <returns>Возвращает расстояние</returns>
-        public static float PointDistanceFloat(PointStructFloat pointOne, PointStructFloat pointTwo)
+        public static float PointDistanceStructFloat(PointStructFloat pointStructFloatOne, PointStructFloat pointStructFloatTwo)
         {
-            float x = pointOne.X - pointTwo.X;
-            float y = pointOne.Y - pointTwo.Y;
+            float x = pointStructFloatOne.X - pointStructFloatTwo.X;
+            float y = pointStructFloatOne.Y - pointStructFloatTwo.Y;
             return MathF.Sqrt((x * x) + (y * y));
         }
 
         /// <summary>
         /// Метод расчёта расстояния между точкама для значимых типов Double
         /// </summary>
-        /// <param name="pointOne">Первая точка</param>
-        /// <param name="pointTwo">Вторая точка</param>
+        /// <param name="pointStructDoubleOne">Первая точка</param>
+        /// <param name="pointStructDoubleTwo">Вторая точка</param>
         /// <returns>Возвращает расстояние</returns>
-        public static double PointDistanceDouble(PointStructDouble pointOne, PointStructDouble pointTwo)
+        public static double PointDistanceStructDouble(PointStructDouble pointStructDoubleOne, PointStructDouble pointStructDoubleTwo)
         {
-            double x = pointOne.X - pointTwo.X;
-            double y = pointOne.Y - pointTwo.Y;
+            double x = pointStructDoubleOne.X - pointStructDoubleTwo.X;
+            double y = pointStructDoubleOne.Y - pointStructDoubleTwo.Y;
             return Math.Sqrt((x * x) + (y * y));
         }
 
@@ -85,13 +135,13 @@ namespace JobLessonAlgo03v02Part01
         /// Метод расчёта расстояния между точкама для значимых типов Float 
         /// без вычисления квадратного корня
         /// </summary>
-        /// <param name="pointOne">Первая точка</param>
-        /// <param name="pointTwo">Вторая точка</param>
+        /// <param name="pointStructFloatWithoutSQRTOne">Первая точка</param>
+        /// <param name="pointStructFloatWithoutSQRTTwo">Вторая точка</param>
         /// <returns>Возвращает расстояние без вычисления квадратного корня</returns>
-        public static float PointDistanceShort(PointStructWithoutSQRT pointOne, PointStructWithoutSQRT pointTwo)
+        public static float PointDistanceStructWithoutSQRT(PointStructWithoutSQRT pointStructFloatWithoutSQRTOne, PointStructWithoutSQRT pointStructFloatWithoutSQRTTwo)
         {
-            float x = pointOne.X - pointTwo.X;
-            float y = pointOne.Y - pointTwo.Y;
+            float x = pointStructFloatWithoutSQRTOne.X - pointStructFloatWithoutSQRTTwo.X;
+            float y = pointStructFloatWithoutSQRTOne.Y - pointStructFloatWithoutSQRTTwo.Y;
             return (x * x) + (y * y);
         }
 
@@ -101,8 +151,7 @@ namespace JobLessonAlgo03v02Part01
         [Benchmark]
         public void TestDistanceClassFloat()
         {
-            
-            //PointDistanceDouble();
+            DistanceClassFloat();
         }
 
         /// <summary>
@@ -111,7 +160,7 @@ namespace JobLessonAlgo03v02Part01
         [Benchmark]
         public void TestDistanceStructFloat()
         {
-
+            DistanceStructFloat();
         }
 
         /// <summary>
@@ -120,7 +169,7 @@ namespace JobLessonAlgo03v02Part01
         [Benchmark]
         public void TestDistanceStructDouble()
         {
-
+            DistanceStructDouble();
         }
 
         /// <summary>
@@ -130,7 +179,7 @@ namespace JobLessonAlgo03v02Part01
         [Benchmark]
         public void TestDistanceStructFloatWithoutSQRT()
         {
-
+            DistanceStructFloatWothoutSQRT();
         }
     }
 }
---
+++ JobLessonAlgo03v02Part01/PointStructDouble.cs
@@ -0,0 +1,14 @@
+﻿namespace JobLessonAlgo03v02Part01
+{
+    public partial class BechmarkClass
+    {
+        /// <summary>
+        /// Значимый тип обозначения координат точки типа Double
+        /// </summary>
+        public struct PointStructDouble
+        {
+            public double X;
+            public double Y;
+        }
+    }
+}
